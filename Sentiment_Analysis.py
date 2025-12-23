286e837c6daac1f667c6872c3f8b757eae237d62
Zihadul Talukder <70834486+zihadulislam99@users.noreply.github.com>
2025-12-23 12:51:54 +0600

    Add files via upload

---
+++ Sentiment_Analysis.py
@@ -0,0 +1,26 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
+
+MODEL_PATH = "./Sentiment_Analysis/sentiment_model"
+tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH, local_files_only=True)
+model = AutoModelForSequenceClassification.from_pretrained(MODEL_PATH, local_files_only=True)
+model.eval()  # important for inference
+
+def predict_sentiment(texts):
+    if isinstance(texts, str):
+        texts = [texts]
+    inputs = tokenizer(texts, return_tensors="pt", truncation=True, padding=True, max_length=512)
+
+    with torch.no_grad():
+        outputs = model(**inputs)
+    probs = torch.nn.functional.softmax(outputs.logits, dim=-1)
+    sentiment_map = {0: "Very Negative", 1: "Negative", 2: "Neutral", 3: "Positive", 4: "Very Positive"}
+    # sentiment_map = {0: -2, 1: -1, 2: 0, 3: 1, 4: 2}
+    return [sentiment_map[i] for i in torch.argmax(probs, dim=-1).tolist()]
+
+texts = ["Ti amo", "I loVe you"]
+
+print(predict_sentiment("সেবার মান একেবারেই খারাপ।")[0])
+print(predict_sentiment(["Ti amo", "I loVe you"]))
+for text, sentiment in zip(texts, predict_sentiment(texts)):
+    print(f"Text: {text}\nSentiment: {sentiment}\n")
